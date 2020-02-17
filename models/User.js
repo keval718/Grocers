2d03e56936aa5be1a2336bb29cb4353b2d389d5f
keval718 <kevalvirat@gmail.com>
2020-02-17 03:10:17 -0500

    User Api Completed Using Authentication (Insert/Login)

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose');
+
+const UserScheme=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email: {
+         type:String,
+         required:true
+    },
+    password:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    street_address:{
+        type:String,
+        required:true
+    },
+    postal_code:{
+        type:String,
+        required:true
+    },
+    province:{
+        type:String,
+        required:true
+    },
+    country:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    }
+
+});
+
+const User=mongoose.model('user',UserScheme);
+module.exports=User;
---
+++ routes/verifiedtoken.js
@@ -0,0 +1,24 @@
+const jwt=require('jsonwebtoken');
+const config = require('config');
+module.exports=function (req,res,next)
+{
+    const token=req.header('auth-token');
+    console.log(token);
+    if(!token)
+    {
+        return res.send("Access denied");
+
+    }
+    try{
+console.log("inside");
+        const verified =jwt.verify(token,config.get('jwtsecret'));
+        console.log(verified);
+
+        req.user=verified;
+        next();
+    }
+    catch(err)
+    {
+        res.send(err);
+    }
+}
